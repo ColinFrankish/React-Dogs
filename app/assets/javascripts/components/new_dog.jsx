6ae8a8dd2d1110a89a383fa24098929704bde196
Colin Frankish <colin.frankish@gmail.com>
2017-09-27 17:23:54 +0100

    New Dog input and submit fields

--- app/assets/javascripts/components/new_dog.jsx
+++ app/assets/javascripts/components/new_dog.jsx
@@ -3,6 +3,9 @@ var NewDog= React.createClass({
         return (
             <div>
                 <h1>New Dog</h1>
+                <input ref='name' placeholder='Enter the name of the canine' /><br/>
+                <input ref='wagRate' placeholder='Enter a wag rate between 1-100' /><br/>
+                <button>Submit</button>
             </div>
         )
     }
