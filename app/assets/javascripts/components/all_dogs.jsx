370ba134685de707d14b46b35e133702fc9221b0
Colin Frankish <colin.frankish@gmail.com>
2017-10-03 15:20:08 +0100

    Thinking about how to style, couple of starters to ponder

--- app/assets/javascripts/components/all_dogs.jsx
+++ app/assets/javascripts/components/all_dogs.jsx
@@ -7,9 +7,17 @@ class AllDogs extends React.Component {
 
   render() {
 
+    let styles = {
+      allDogs: {
+        width: "25%", 
+        textAlign: "center"
+      }
+    }
+    console.log('spliced dogs:',this.props.allDogs.splice(0,3))
     let dogs = this.props.allDogs.map((dog) => {
       return (
-        <div key={dog.id}>
+        <div  key={dog.id}
+              style={styles.allDogs}>
           <h3>Name: {dog.name}</h3>
           <p>Wag Rate: {dog.tail_wag_rate}</p>
           <button onClick={this.props.handleDeleteDog.bind(this, dog.id, dog.name)}>Destroy {dog.name} !</button>
